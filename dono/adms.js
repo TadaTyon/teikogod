f372ad6e95015b0656653fb2594daf3d02b793fb
TadaTyon <ed.thalyonii@gmail.com>
2022-01-14 18:51:07 -0300

    first commit

---
+++ dono/adms.js
@@ -0,0 +1,32 @@
+const adms = (prefix) => { 
+	return `   
+━━━━Menu do Tyon━━━━
+◆◇
+◆◇ ${prefix}resetavelha
+◆◇ ${prefix}ban (mencionar mensg pra rmv)
+◆◇ ${prefix}kick [@] (vai remover o cara)
+◆◇ ${prefix}grupo f/a
+◆◇ ${prefix}add [@] (para adc alguém)
+◆◇ ${prefix}linkgp
+◆◇ ${prefix}promover [@] (promover a adm)
+◆◇ ${prefix}rebaixar [@] (rebaixar adm)
+◆◇ ${prefix}grupoinfo
+◆◇ ${prefix}hidetag (txt) (Marc tds invsv)
+◆◇ ${prefix}marcar (marca tds do gp)
+◆◇ ${prefix}marcar1 (marca tds do gp)
+◆◇ ${prefix}anagrama 1 / 0
+◆◇ ${prefix}antidocumento 1 / 0  
+◆◇ ${prefix}anticatalogo 1 / 0
+◆◇ ${prefix}antiracismo 1 / 0
+◆◇ ${prefix}anticontato 1 / 0  
+◆◇ ${prefix}antiloc 1 / 0  
+◆◇ ${prefix}leveling 1 / 0  
+◆◇ ${prefix}antilink 1 / 0
+◆◇ ${prefix}antifake 1 / 0
+◆◇ ${prefix}bemvindo 1 / 0
+◆◇ ${prefix}simih 1 / 0
+◆◇
+━━━━❮ ${NamaBot} ❯━━━━`
+
+}
+exports.adms = adms
